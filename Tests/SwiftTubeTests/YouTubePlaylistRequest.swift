886185bafc0e67f0b83c987c202c104ad8e071fd
Leo Dion <leogdion@brightdigit.com>
2022-02-15 09:57:29 -0500

    fixing and refacotring unit tests

---
+++ Tests/SwiftTubeTests/YouTubePlaylistRequest.swift
@@ -0,0 +1,22 @@
+import Prch
+import SwiftTube
+import XCTest
+
+// typealias YouTubeVideo = SwiftTube.Video
+
+struct YouTubePlaylistRequest {
+  internal init(apiKey: String, playlistID: String) {
+    self.apiKey = apiKey
+    self.playlistID = playlistID
+  }
+
+  let apiKey: String
+  let playlistID: String
+}
+
+extension YouTubePlaylistRequest {
+  init(settings: Settings) {
+    precondition(settings.playlistID != nil || settings.apiKey != nil)
+    self.init(apiKey: settings.apiKey!, playlistID: settings.playlistID!)
+  }
+}
