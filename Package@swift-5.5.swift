fd0e517ddca82303b1d65187a38d5c3044c70394
Leo Dion <leogdion@brightdigit.com>
2022-06-29 21:31:27 -0400

    Adding Prch submodule

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -18,8 +18,8 @@ let package = Package(
     .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
     .package(url: "https://github.com/realm/SwiftLint", from: "0.41.0"), // dev
     .package(url: "https://github.com/shibapm/Rocket", from: "1.2.0"), // dev
-    .package(url: "https://github.com/brightdigit/swift-test-codecov", from: "1.0.0"), // dev    ,
-    .package(url: "https://github.com/brightdigit/Prch.git", from: "0.2.0")
+    .package(url: "https://github.com/brightdigit/swift-test-codecov", from: "1.0.0"), // dev
+    .package(path: "Packages/Prch")
   ],
   targets: [
     .target(name: "SwiftTube", dependencies: ["Prch"]),
